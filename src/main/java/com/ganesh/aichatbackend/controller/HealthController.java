0043510371cef2cb9765b15d0239d92233d02157
ganesh11499 <ganeshramesh11499@gmail.com>
2026-06-15 19:43:03 +0530

    deploye changes

--- src/main/java/com/ganesh/aichatbackend/controller/HealthController.java
+++ src/main/java/com/ganesh/aichatbackend/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.ganesh.AiChatBackend.controller;
+package com.ganesh.aichatbackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
